8898a1d44fefeb094d66cd2b8960cb5949687355
alexander.maskovyak <alexander.maskovyak@cf9d9ebc-924b-0410-a049-f1d884abcb7f>
2008-05-08 03:49:14 +0000

    AGM: Fixed some minor bugs...minor in that they caused Sequential to be completely non-functional.

--- trunk/v2/GameOfLife.java
+++ trunk/v2/GameOfLife.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import edu.rit.pj.Comm;
 
 /**
  * Handles the execution of the game of life.
@@ -10,30 +14,100 @@ public class GameOfLife {
 	/**
 	 * Main executable method for the game of life.
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		// 
+		// Setup Parallel Java objects
+		//
+		Comm.init(args);
+		Comm commWorld = Comm.world();
+		
+		//
+		// read inputs from main
+		//
+		int numGridRows = Integer.parseInt(args[0]);
+		int numGridCols = Integer.parseInt(args[1]);
+		int iterationsToRun = Integer.parseInt(args[2]);
+		String liveCellInputFile = args[3];
+		
+		//
 		// create Game of Life objects
-		// specify which rules to use
-		// specify which interconnection framework
+		//
+		
+		// specify rules to use
+		// specify interconnection framework
 		// specify simulation environment
 		CellLifeRules rules = new ConwayCellLifeRules();
-		CellNeighborhood neighborhood = new ConwayCellNeighborhood();
-		GameOfLifeSimulator simulator = 
-			new SequentialGameOfLifeSimulator(rules, neighborhood);
-		
+		CellNeighborhood neighborhood = 
+			new ConwayCellNeighborhood(0, numGridCols, 0, numGridRows);
+		File cellsFile = new File("glider.txt");
+
 		// create timing items
 		long startTime, endTime, runTime;
+
+		//System.out.println("Getting live cells.");
 		
-		// run the glider simulation as an example
+		List<Cell> livingCells = GameOfLifeFileIO.getLiveCells(cellsFile);
+		
+		//System.out.printf("Have live cells: %d\n", livingCells.size());
+		
+		// create objects, distribute data and perform simulation
+		// better start our time
 		startTime = System.nanoTime();
 		
-		GameOfLife.gliderTestCase(simulator);
+		//System.out.println("Make simulators...");
+		
+		GameOfLifeSimulator simulator = 
+			new NeighborCommGameOfLifeSimulator(
+					rules, 
+					neighborhood, 
+					commWorld,
+					cellsFile);
+		
+		
+		System.out.printf("%d cells alive\n", simulator.getLivingCellCount());
+		for (int i = 0; i < 1; ++i) {
+			simulator.performSimulation();
+			//System.out.printf("Iteration %d, %d cells alive\n", i, simulator.getLivingCellCount());
+			System.out.printf("Iteration %d, %d: %s cells alive\n", i, commWorld.rank(), simulator.getLivingCellCount());
+		}	
+		
+		//System.out.printf("%d has %d cells alive\n", commWorld.rank(), simulator.getLivingCellCount());
+		
+		for (Cell c : simulator.getCurrentState()) {
+			System.out.printf("%d: %s\n", commWorld.rank(), c);
+		}
 		
 		// display running time
 		endTime = System.nanoTime();
 		runTime = endTime - startTime;
 		System.out.printf("Runtime: %dms\n", 
 				convertNanoSecondsToMilliseconds(runTime));
+		
+		
+		
+		// for testing purposes
+//		List<Cell> liveCells = simulator.getCurrentState();/
+//		for (Cell c : liveCells) {
+//			System.out.printf("%d: %s\n", commWorld.rank(), c);
+//		}
+
+		// run the glider simulation as an example
+//		startTime = System.nanoTime();
+//		GameOfLife.gliderTestCase(simulator);
+
+
+		
+		/*		List<Cell> liveCells = GameOfLifeFileIO.getLiveCells("glider.txt");
+		Cell[] liveCellArray = liveCells.toArray(new Cell[liveCells.size()]);
+		//java.util.Arrays.sort(liveCellArray, Cell.ColumnComparator);
+		
+		for (Cell cell : liveCellArray) {
+			System.out.printf("%s\n", cell.toString());
+		}
+*/
+
 	}
 
 	/**
