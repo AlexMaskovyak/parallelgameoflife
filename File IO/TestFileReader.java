181cbc7cb22eab82b8a52d48b3c5b0bb8f2b6577
stevebaylor <stevebaylor@gmail.com>
2008-05-11 21:22:49 +0000

    Updated File IO to work with real Cell class

--- File IO/TestFileReader.java
+++ File IO/TestFileReader.java
@@ -3,16 +3,17 @@ import java.util.ArrayList;
 
 public class TestFileReader {
 	public static void main (String[] args){
-		
 		List CellList = new ArrayList<Cell>();
+		
 		
 		CellList = CellFileReader.readFile( args[0] );
 		
 		Cell NextCell;
-		while ( ! CellList.isEmpty() ){
+		//for ( Cell NextCell: CellList ){
+		while( !CellList.isEmpty() ){
 			NextCell = (Cell)CellList.remove(CellList.size() - 1 );
-			System.out.print("<" + NextCell.getRow() + "," +
-					NextCell.getCol() + "> " );
+			System.out.print("<" + NextCell.x + "," +
+					NextCell.y + "> " );
 		}
 	}
 }
